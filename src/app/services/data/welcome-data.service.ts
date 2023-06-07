59684e7ea2e76f503b0b5554c5dd14cafaaf9159
ken0901 <ken5260715@gmail.com>
2023-06-06 21:59:04 -0700

    Enhancing service to use basic Auth for Welcome

--- src/app/services/data/welcome-data.service.ts
+++ src/app/services/data/welcome-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
@@ -17,6 +17,20 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name){
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    });
+
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`
+                                          ,{headers: headers}
+                                        );
+  }
+
+  createBasicAuthenticationHttpHeader(){
+    let username = 'ken';
+    let password = 'lee';
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
   }
 }
