c95a3d78dc634f7760929bf9eac1ac59a701c026
ken0901 <ken5260715@gmail.com>
2023-06-07 22:16:47 -0700

    Use Constants for URLs and Tokens

--- src/app/services/basic-authentication.service.ts
+++ src/app/services/basic-authentication.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { API_URL } from '../constants/app.constants';
+
+export const TOKEN = 'token';
+export const AUTHENTICATED_USER = 'authenticaterUser';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +16,7 @@ export class BasicAuthenticationService {
   // hardcoded 
   authenticate(username, password){
     if(username ==="Ken" && password === "lee"){
-      sessionStorage.setItem('authenticaterUser',username);
+      sessionStorage.setItem(AUTHENTICATED_USER,username);
       return true;
     }else{
       return false;
@@ -20,24 +24,24 @@ export class BasicAuthenticationService {
   }
 
   getAuthenticatedUser(){
-    return sessionStorage.getItem('authenticaterUser');
+    return sessionStorage.getItem(AUTHENTICATED_USER);
   }
 
   getAuthenticatedToken(){
     if(this.getAuthenticatedUser()){
-      return sessionStorage.getItem('token');
+      return sessionStorage.getItem(TOKEN);
     }
     return null;
   }
 
   isUserLoggedIn(){
-    let user = sessionStorage.getItem('authenticaterUser');
+    let user = sessionStorage.getItem(AUTHENTICATED_USER);
     return !(user === null);
   }
 
   logout(){
-    sessionStorage.removeItem('authenticaterUser');
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem(AUTHENTICATED_USER);
+    sessionStorage.removeItem(TOKEN);
   }
 
   executeAuthenticationService(username, password){
@@ -47,13 +51,13 @@ export class BasicAuthenticationService {
       Authorization: basicAuthHeaderString
     });
 
-    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`
+    return this.http.get<AuthenticationBean>(`${API_URL}/basicauth`
                                           ,{headers: headers}
                                         ).pipe(
                                           map(
                                             data => {
-                                              sessionStorage.setItem('authenticaterUser', username);
-                                              sessionStorage.setItem('token', basicAuthHeaderString);
+                                              sessionStorage.setItem(AUTHENTICATED_USER, username);
+                                              sessionStorage.setItem(TOKEN, basicAuthHeaderString);
                                               return data;
                                             }
                                           )
