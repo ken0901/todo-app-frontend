886149dbc6cc60aca208e0e7d98ebf4cb887659e
ken0901 <ken5260715@gmail.com>
2023-06-07 21:04:26 -0700

    Configure Http Interceptor to add basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
@@ -14,6 +14,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { ErrorComponent } from './error/error.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpIntercepterBasicAuthService } from './services/http/http-intercepter-basic-auth.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,9 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBasicAuthService, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/http/http-intercepter-basic-auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpIntercepterBasicAuthService implements HttpInterceptor{
+  
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let username = 'ken';
+    let password = 'lee';
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    req = req.clone({
+      setHeaders : {
+        Authorization : basicAuthHeaderString
+      }
+    });
+
+    return next.handle(req);
+  }
+}
